4495dfc5a1b21d9ac5ba42b28bf3550646f4bd41
Mathew Shardin <mathew.shardin@student.nhlstenden.com>
2021-12-03 16:38:49 +0100

    Back-end Reservation step 1
    Completed back end for reservation step 1 page.
    Personal data input by the user gets saved into the DB in CUSTOMER table

--- reserve1.php
+++ reserve1.php
@@ -92,12 +92,72 @@
 
                     <input type="submit" name="submit" value="Next" id="reserveNextButton">
                 </form>
+
+
+                <?php
+                if ($_SERVER['REQUEST_METHOD']=='POST') {
+                    //Validate all input data
+                    if (!empty($_POST['email']) AND $email=filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL)) {
+                        if (!empty($_POST['name']) AND !empty($_POST['lastname']) AND !empty($_POST['address'])) {
+                            //If all inputs are valid
+                            //Assign variables not to adress superglobals directly
+                            $name=filter_input(INPUT_POST, 'name');
+                            $lastname=filter_input(INPUT_POST, 'lastname');
+                            $address=filter_input(INPUT_POST, 'address');
+                            //Concatenate the year, month, date of birth to satisfy mySQLi date format
+                            $dob=filter_input(INPUT_POST, 'dobYear').".".filter_input(INPUT_POST, 'dobMonth').".".filter_input(INPUT_POST, 'dobDay');
+
+
+                            //Check if a CUSTOMER with this email already exists
+                            if ($conn = mysqli_connect("localhost", "root", "")) { //Connect to DB and select the DB
+                                mysqli_select_db($conn, "hp_reserved");
+
+                                //Check if the input email already exists
+                                $sql = 'SELECT * FROM `customer` WHERE custEmail = ?';
+                                if ($stmt = mysqli_prepare($conn, $sql)) {
+
+                                    mysqli_stmt_bind_param($stmt, 's', $email);
+
+                                    //Execute statement if preparation is successful
+                                    if (mysqli_stmt_execute($stmt)) {
+                                    } else {echo "<div class='reservePHPResponse'><p>Submission error. Try again later</p></div>";}
+
+                                } else {echo "<div class='reservePHPResponse'><p>Connection check error. Try again later</p></div>";}
+
+                                mysqli_stmt_store_result($stmt);
+                                if (mysqli_stmt_num_rows($stmt) == 0) {
+                                    //If the CUSTOMER with this email is not present in the DB add it to the DB
+                                    $sql = 'INSERT INTO `CUSTOMER` VALUES (?,?,?,?,?)';
+                                    if ($stmt = mysqli_prepare($conn, $sql)) {
+
+                                        mysqli_stmt_bind_param($stmt, 'sssss', $email, $name, $lastname, $address, $dob);
+                
+                                        //Execute statement if preparation is successful
+                                        if (mysqli_stmt_execute($stmt)) {
+                                            //Redirect user to next page
+                                            echo '<script type="text/javascript">location.href = "reserve2.php";</script>';
+                                        } else {echo "<div class='reservePHPResponse'><p>Connection error. Try again later</p></div>";}
+                
+                                    } else {echo "<div class='reservePHPResponse'><p>Connection error. Try again later</p></div>";}
+
+                                } else {
+                                    //Redirect user to next page even if CUSTOMER with this email already exists
+                                    echo '<script type="text/javascript">location.href = "reserve2.php";</script>';
+                                }
+
+
+                                //Close connection & Statement
+                                mysqli_stmt_close($stmt);
+                                mysqli_close($conn);
+
+                            } else {echo "<div class='reservePHPResponse'><p>Connection error. Try again later</p></div>";}
+
+
+                        } else {echo "<div class='reservePHPResponse'><p>Please fill out all the fields</p></div>";}
+                    } else {echo "<div class='reservePHPResponse'><p>Please provide a valid e-mail address</p></div>";}
+                }
+                ?>
             </div>
-
-
-            <?php
-            
-            ?>
         </div>
     </body>
 </html>
