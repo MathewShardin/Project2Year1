479d9f207e145d7dfb1a56d9ec5a757a038d9080
Costache Florian-Alin <91316565+Fluffkin23@users.noreply.github.com>
2021-12-13 11:46:38 +0100

    update employeelogin
    update

--- employeeLogin.php
+++ employeeLogin.php
@@ -1,5 +1,5 @@
 <?php
-$conn= mysqli_connect("localhost","root","","user") or
+$conn= mysqli_connect("localhost","root","","hp_reserved") or
 die("Connection Failed" .mysqli_connect_error());
 if(!$conn)
 {
@@ -9,7 +9,7 @@ if($_SERVER["REQUEST_METHOD"]=="POST")
 {
     $EmployeeID=$_POST["EmployeeID"];
     $EmployeePassword=$_POST["EmployeePassword"];
-    $sql="SELECT * from login where username='".$EmployeeID."'AND password='".$EmployeePassword."' ";
+    $sql="SELECT * from hpstaff where username='".$EmployeeID."'AND password='".$EmployeePassword."' ";
     $result= mysqli_query($conn, $sql);
     $row= mysqli_fetch_array($result);
     if($row["usertype"]=="user")
