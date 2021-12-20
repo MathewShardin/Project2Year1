e5052d9ba30d19196d69345091be3d8f5bdbd21d
Mathew Shardin <mathew.shardin@student.nhlstenden.com>
2021-12-20 13:24:12 +0100

    Added a log in check

--- reservationsOverview.php
+++ reservationsOverview.php
@@ -1,4 +1,15 @@
 <?php
-//Check if a user is logged in
+//Check if a user is logged in and redirect the user back to login page if not
 session_start();
+if (!$_SESSION['loggedIn'] == True) {
+    echo '<script type="text/javascript">location.href = "employeeLogin.html";</script>';
+}
 ?>
+<html lang="en-US">
+    <head>
+        <title>H&P - Staff</title>
+        <link href = "Style/style.css" type = "text/css" rel = "stylesheet" >
+    </head>
+    <body>
+    </body>
+</html>
