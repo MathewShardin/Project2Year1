d242bd63be83decd4fc7bc0275ae7f9330d82234
Mathew Shardin <mathew.shardin@student.nhlstenden.com>
2021-12-30 00:21:04 +0100

    Temp changes

--- reservationsOverview.php
+++ reservationsOverview.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <?php
 //Check if a user is logged in and redirect the user back to login page if not
 session_start();
@@ -11,7 +12,9 @@ if (!$_SESSION['loggedIn'] == True) {
         <link href = "Style/style.css" type = "text/css" rel = "stylesheet" >
         <link rel="shortcut icon" type="ico" href="img/favicon.ico"/>
     </head>
+    <!--TEMPORARY CODE. DELETE IT!-->
     Page under construction
+    <a href="staffEvents.php">Events</a>
     <body>
     </body>
 </html>
