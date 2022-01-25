e0876c23ace82fd8aff3e7f1ecc1df59ecbca27f
Mathew Shardin <mathew.shardin@student.nhlstenden.com>
2022-01-25 09:46:40 +0100

    Updated DB

--- hp_reserved.sql
+++ hp_reserved.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 13, 2021 at 10:27 AM
+-- Generation Time: Jan 25, 2022 at 09:46 AM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 8.0.9
 
@@ -47,6 +47,9 @@ INSERT INTO `customer` (`custEmail`, `custName`, `custSurname`, `custAddress`, `
 ('Shardin111@gmail.com', 'Matvei', 'Shardin', 'Monetpassage 23 - 11', '2003-04-25'),
 ('shardin@mail.ru', 'Mathew', 'Shardin', 'Monetpassage 23 - 11', '2000-05-25'),
 ('test2@gmail.com', 'Mike', 'Mo', '137 Strathearn Pl, Simi Valley, CA 93065, United States', '1990-03-27'),
+('test4@gmail.com', 'Elizabeth', 'McDonald', 'Example address - 18, 6709PP', '1998-07-14'),
+('test5@rambler.com', 'Steve', 'Berra', 'Amsterdam, example st. 14 ', '1990-06-13'),
+('test7@gmail.com', 'Luan', 'Oliviera', 'Sample address 45, USA, 78121', '1988-07-20'),
 ('test@test.com', 'Eric', 'Koston', '2535A E 12th St, Los Angeles, CA 90021, USA', '1975-04-29');
 
 -- --------------------------------------------------------
@@ -67,7 +70,8 @@ CREATE TABLE `event` (
 
 INSERT INTO `event` (`eventId`, `eventDate`, `eventName`) VALUES
 (1, '2021-04-20', 'St.Patricks Day'),
-(2, '2022-02-09', 'Test event');
+(2, '2022-02-09', 'Test event'),
+(14, '2022-02-20', 'MJ Day');
 
 -- --------------------------------------------------------
 
@@ -93,7 +97,9 @@ INSERT INTO `guest` (`guestId`, `guestName`, `guestSurname`, `guestDateofBirth`,
 (2, 'Spencer', 'Barton', '1995-04-21', 'Male', 3),
 (3, 'Ana', 'Smith', '1998-06-24', 'female', 10),
 (4, 'Mathew', 'shardin', '2003-04-25', 'male', 11),
-(5, 'John', 'Smith', '2021-12-09', 'male', 14);
+(5, 'John', 'Smith', '2021-12-09', 'male', 14),
+(7, 'Test', 'Guest1', '2021-12-30', 'male', 19),
+(8, 'Test', 'Guest2', '2015-03-17', 'female', 19);
 
 -- --------------------------------------------------------
 
@@ -112,7 +118,8 @@ CREATE TABLE `hpstaff` (
 --
 
 INSERT INTO `hpstaff` (`userName`, `password`, `userType`) VALUES
-('admin', 'admin', 'admin');
+('admin', '$2y$10$An8ZwTcwmP0I21kMGAEs4uxDUsTFF1/i9NIkJCnUskeKBq/ltW19q', 'admin'),
+('test', '$2y$10$qBevkWzp4k2/14wdAw1OLujPXEiXfIegIwqfCEWMfDsvYMNVFDsAC', 'user');
 
 -- --------------------------------------------------------
 
@@ -146,7 +153,10 @@ INSERT INTO `reservation` (`resId`, `custEmail`, `resNumberGuests`, `resCheckIn`
 (10, 'johnsmith@gmail.com', 2, '2022-05-13', 3, 'Extra Pillow', NULL, 'Bamboo', 'Location 2', 'card', 726),
 (11, 'dan@mail.ru', 2, '2022-02-10', 3, 'Set of spices Champagne Bottle', NULL, 'Brick', 'Location 2', 'cash', 1107),
 (13, 'Shardin111@gmail.com', 1, '2023-06-12', 1, '', NULL, 'Straw', 'Location 1', 'cash', 145),
-(14, 'mathew.shardin@student.nhlstenden.com', 2, '2022-02-03', 6, ' Champagne Bottle', 2, 'Brick', 'Location 2', 'cash', 2196);
+(14, 'mathew.shardin@student.nhlstenden.com', 2, '2022-02-03', 6, ' Champagne Bottle', 2, 'Brick', 'Location 2', 'cash', 2196),
+(15, 'Shardin111@gmail.com', 1, '2022-02-08', 2, '', 2, 'Straw', 'Location 1', 'cash', 242),
+(17, 'test4@gmail.com', 1, '2022-03-04', 3, 'Birthday cake', NULL, 'Brick', 'Location 2', 'paypal', 1089),
+(19, 'test7@gmail.com', 3, '2022-01-25', 3, '', NULL, 'Bamboo', 'Location 2', 'paypal', 363);
 
 --
 -- Indexes for dumped tables
@@ -169,7 +179,7 @@ ALTER TABLE `event`
 --
 ALTER TABLE `guest`
   ADD PRIMARY KEY (`guestId`),
-  ADD KEY `resId` (`resId`);
+  ADD KEY `guest_ibfk_1` (`resId`);
 
 --
 -- Indexes for table `hpstaff`
@@ -182,8 +192,8 @@ ALTER TABLE `hpstaff`
 --
 ALTER TABLE `reservation`
   ADD PRIMARY KEY (`resId`),
-  ADD KEY `custEmail` (`custEmail`),
-  ADD KEY `eventId` (`eventId`);
+  ADD KEY `reservation_ibfk_1` (`custEmail`),
+  ADD KEY `reservation_ibfk_2` (`eventId`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -193,19 +203,19 @@ ALTER TABLE `reservation`
 -- AUTO_INCREMENT for table `event`
 --
 ALTER TABLE `event`
-  MODIFY `eventId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `eventId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `guest`
 --
 ALTER TABLE `guest`
-  MODIFY `guestId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `guestId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `reservation`
 --
 ALTER TABLE `reservation`
-  MODIFY `resId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `resId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- Constraints for dumped tables
@@ -215,14 +225,14 @@ ALTER TABLE `reservation`
 -- Constraints for table `guest`
 --
 ALTER TABLE `guest`
-  ADD CONSTRAINT `guest_ibfk_1` FOREIGN KEY (`resId`) REFERENCES `reservation` (`resId`);
+  ADD CONSTRAINT `guest_ibfk_1` FOREIGN KEY (`resId`) REFERENCES `reservation` (`resId`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `reservation`
 --
 ALTER TABLE `reservation`
-  ADD CONSTRAINT `reservation_ibfk_1` FOREIGN KEY (`custEmail`) REFERENCES `customer` (`custEmail`),
-  ADD CONSTRAINT `reservation_ibfk_2` FOREIGN KEY (`eventId`) REFERENCES `event` (`eventId`);
+  ADD CONSTRAINT `reservation_ibfk_1` FOREIGN KEY (`custEmail`) REFERENCES `customer` (`custEmail`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `reservation_ibfk_2` FOREIGN KEY (`eventId`) REFERENCES `event` (`eventId`) ON DELETE SET NULL ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
